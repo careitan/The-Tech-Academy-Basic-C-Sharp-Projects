cfaa969939c0a4474ecaec38386b23a2febdbc6e
Allan Reitan <careitan2000@gmail.com>
2019-04-04 17:03:35 -0700

    Create Program.cs
    Create inital build

---
+++ AcmeAccounting/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Acme
+{
+	static void Main()
+	{
+		Console.WriteLine("Welcome to Acme Accounting Systems");
+		Console.WriteLine("Remember, we’re \"accounting\" on you!");
+	}
+}
