a74b77c1d46294865147fb18a1979bfeb7cfce35
Allan Reitan <careitan2000@gmail.com>
2019-04-04 19:29:08 -0700

    Create Program.cs
    Inital build of Program.cs

---
+++ VarsAndData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VarsAndData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CourseName = string.Empty;
+            string NeedHelp = string.Empty;
+            string PositiveExperience = string.Empty;
+            string Feedback = string.Empty;
+
+            int PageNumber = 0;
+
+            decimal HoursOfStudy = 0.0m;
+
+            // Start displaying Content
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+
+
+        }
+    }
+}
