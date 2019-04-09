94164ebfa8c07da226de49e6a54bbf9d2ad23211
Allan Reitan <careitan2000@gmail.com>
2019-04-09 15:25:14 -0700

    Build Anon Income progrsm
    Build inital build.

---
+++ AnonIncomeCompare/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonIncomeCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Hours1, Rate1, Hours2, Rate2;
+
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1\nHourly Rate?");
+
+            int.TryParse(Console.ReadLine(), out Rate1);
+
+            Console.WriteLine("Hours worked per week?");
+            int.TryParse(Console.ReadLine(), out Hours1);
+
+            Console.WriteLine("Person 2\nHourly Rate?");
+
+            int.TryParse(Console.ReadLine(), out Rate2);
+
+            Console.WriteLine("Hours worked per week?");
+            int.TryParse(Console.ReadLine(), out Hours2);
+
+            Console.WriteLine("Weekly salary of Person 1:\n" + (Rate1 * Hours1).ToString());
+
+            Console.WriteLine("Weekly salary of Person 2:\n" + (Rate2 * Hours2).ToString());
+
+            Console.WriteLine("Does Person 1 make more than Person 2?");
+
+            bool result = ((Rate1 * Hours1) > (Rate2 * Hours2)) ? true : false;
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
