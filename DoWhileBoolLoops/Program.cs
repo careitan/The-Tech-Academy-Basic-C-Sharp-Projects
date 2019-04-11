6a0faa7ad32e5b7b45b99f83cd14126ac088ba52
Allan Reitan <careitan2000@gmail.com>
2019-04-11 14:48:52 -0700

    Build While Loops
    Build While Loops

---
+++ DoWhileBoolLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DoWhileBoolLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Control = 0;
+
+            while (Control < 3)
+            {
+                Console.Write("Do... ");
+
+                int i = 1;
+                do
+                {
+                    if (Control < 2) { Console.Write("Be... "); } else { Console.Write("\n"); }
+                    i++;
+                } while (i<2);
+
+                Control++;
+            }
+        }
+    }
+}
