1b815e9b82db1f18b2a54c7afd115994b74c88d6
Allan Reitan <careitan2000@gmail.com>
2019-04-18 13:57:06 -0700

    DUN - Done
    DUN

--- FunWithStrings/Program.cs
+++ FunWithStrings/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FunWithStrings
@@ -28,14 +27,27 @@ namespace FunWithStrings
             Value3 = @"miney-mo";
             ConCat = Value1 + Value2 + Value3;
 
-            Console.WriteLine("Here are three little strkngs:");
+            Console.WriteLine("Here are three little strings:");
             Console.Write(Value1 + "; " + Value2 + "; ");
             Console.WriteLine(Value3 + "; ");
 
-            Console.WriteLine("For my first trick I will concatinate them together: " + ConCat);
-            Console.WriteLine("For my next trick I will convert something to uppercase: " + ConCat.ToUpper());
+            Console.WriteLine("\nFor my first trick I will concatinate them together: " + ConCat);
+            Console.WriteLine("\nFor my next trick I will convert something to uppercase: " + ConCat.ToUpper());
 
+            Console.WriteLine("\nOk, now lets try to build a paragraph, one phrase at a time...");
+            Thread.Sleep(2000);
+            Console.Write("\nReady...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Begin!");
 
+            StringBuilder sb = new StringBuilder();
+            sb.Clear();
+            for (int i = 0; i < Independent.Length; i++)
+            {
+                sb.Append(Independent[i]);
+                Console.WriteLine("\n" + sb.ToString());
+                Thread.Sleep(5000);
+            }
 
         }
     }
