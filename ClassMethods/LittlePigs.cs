f7339ea44f643cc3dc3ef4b4fbb367e858430344
Allan Reitan <careitan2000@gmail.com>
2019-04-19 17:45:13 -0700

    Three Little Methods
    Build Three little methods.

---
+++ ClassMethods/LittlePigs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    public static class LittlePigs
+    {
+        public static int Straw(int input)
+        {
+            return input / 3;
+        }
+
+        public static int Wood(int input)
+        {
+            return 7 * input;
+        }
+
+        public static int Brick(int input)
+        {
+            return input * input;
+        }
+
+    }
+}
---
+++ ClassMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = string.Empty;
+            int MyNum = int.MinValue;
+
+            try
+            {
+                Console.WriteLine("And now for something completely different, please give me a number:");
+                result = Console.ReadLine();
+                MyNum = int.Parse(result);
+
+                Console.WriteLine("Method #1: " + LittlePigs.Straw(MyNum));
+                Console.WriteLine("Method #2: " + LittlePigs.Wood(MyNum));
+                Console.WriteLine("Method #3: " + LittlePigs.Brick(MyNum));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
