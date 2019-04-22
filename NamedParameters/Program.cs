f33e6363f9777a6be60d09193feb382ffbc6f2d1
Allan Reitan <careitan2000@gmail.com>
2019-04-22 09:03:13 -0700

    Build Methods
    Build Methods

---
+++ NamedParameters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Something.DoSomething(1, 2);
+            Something.DoSomething(Value2: 6, Value1: 3);
+        }
+    }
+}
---
+++ NamedParameters/Something.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    public class Something
+    {
+        public static void DoSomething(int Value1, int Value2)
+        {
+            int Void = Math.Abs(Value1);
+
+            Console.WriteLine("Here was the second Value that you gave me: {0}", Value2);
+        }
+
+    }
+}
