c03e879d6999b3675c170de484bc77a37b0eb88e
Allan Reitan <careitan2000@gmail.com>
2019-04-22 10:11:55 -0700

    DUN - Done
    DUN - Done

---
+++ OptionalParamsUserInput/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParamsUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MyNum1, MyNum2, Output;
+            string result = "null";
+
+            Console.WriteLine("Please give me one or two numbers to perform Math operstions.");
+            while (!int.TryParse(result, out MyNum1))
+            {
+                Console.Write("First Value: ");
+                result = Console.ReadLine();
+            }
+
+            Console.Write("Second Value [Enter for no value]: ");
+            result = Console.ReadLine();
+
+            Output = (int.TryParse(result, out MyNum2)) ?
+                Something.DoSomething(MyNum1, MyNum2) :
+                Something.DoSomething(MyNum1);
+
+            Console.WriteLine("The Output value is: {0}", Output);
+        }
+    }
+}
---
+++ OptionalParamsUserInput/Something.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParamsUserInput
+{
+    public class Something
+    {
+        public static int DoSomething(int Value1, int Value2 = 3)
+        {
+            return (Value1 + 200) / Value2;
+        }
+
+    }
+}
