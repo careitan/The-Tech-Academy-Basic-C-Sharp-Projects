804014941382c61e2493a6c3fa860dc64fb16618
Allan Reitan <careitan2000@gmail.com>
2019-04-22 11:05:50 -0700

    DUN - Done
    DUN - Done

---
+++ OutputParameters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Something MyWhatever = new Something();
+            string result = string.Empty;
+            int MyNum = 0;
+            int CatchValue = 0;
+
+            Console.Write("Please give me a number to do something with it: ");
+            int.TryParse(Console.ReadLine(), out MyNum);
+
+            Something.DoSomething(MyNum);
+            Something.DoSomething(MyNum, out CatchValue);
+            Console.WriteLine("The CatchValue is: {0}", CatchValue);
+        }
+    }
+}
---
+++ OutputParameters/Something.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputParameters
+{
+    class Something
+    {
+        public static void DoSomething(int Value)
+        {
+            Console.WriteLine("The output result is: {0}", Value / 2);
+        }
+
+        public static void DoSomething(int Value, out int Output)
+        {
+            Output = Value / 2;
+        }
+    }
+}
