67f9f6734dae8acece04c1fcce556022fb6cfb9f
Allan Reitan <careitan2000@gmail.com>
2019-04-22 09:15:59 -0700

    DUN - Done
    DUN - Done

---
+++ ThreeVariationsOfMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeVariationsOfMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MyNum = 42;
+            decimal MyDec = 3.14m;
+            string ANum = "12";
+            string BadNum = "Hello World";
+
+            Console.WriteLine("Results for INT: {0}", Something.DoSomething(MyNum));
+            Console.WriteLine("Results for DEC: {0}", Something.DoSomething(MyDec));
+            Console.WriteLine("Results for String-NUM: {0}", Something.DoSomething(ANum));
+            Console.WriteLine("Results for String-Word: {0}", Something.DoSomething(BadNum));
+
+        }
+    }
+}
---
+++ ThreeVariationsOfMethods/Something.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeVariationsOfMethods
+{
+    public class Something
+    {
+        public static int DoSomething(int Value)
+        {
+            return Value * 6;
+        }
+
+        public static int DoSomething(decimal Value)
+        {
+            return (int)Value + 6;
+        }
+
+        public static int DoSomething(string Value)
+        {
+            int result = int.MinValue;
+
+            if(int.TryParse(Value, out result))
+            {
+                return result * result;
+            }
+
+            return result;
+        }
+    }
+}
