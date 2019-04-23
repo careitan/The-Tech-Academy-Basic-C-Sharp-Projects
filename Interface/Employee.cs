c0dcd85531f2eebd7f31c243760614d0e87129d9
Allan Reitan <careitan2000@gmail.com>
2019-04-23 13:39:03 -0700

    DUN - Done
    DUN - Done

---
+++ Interface/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEmployee
+{
+    public class Employee:Person, IQuittable
+    {
+        int ID;
+
+        public void Quit()
+        {
+            Console.WriteLine("{0} {1} says, 'I QUIT!'",this.FirstName,this.LastName);
+        }
+    }
+}
