f25c217d77001d9196aca2d6669d3f672d1e9818
Allan Reitan <careitan2000@gmail.com>
2019-04-23 14:48:49 -0700

    DUN - Done
    DUN - Done

---
+++ OverloadedOperator/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEmployee
+{
+    public class Employee : Person, IQuittable
+    {
+        public int ID;
+
+        public void Quit()
+        {
+            Console.WriteLine("{0} {1} says, 'I QUIT!'", this.FirstName, this.LastName);
+        }
+
+        public static bool operator==(Employee Employee1, Employee Employee2)
+        {
+            return Employee1.ID == Employee2.ID;
+        }
+
+        public static bool operator!=(Employee Employee1, Employee Employee2)
+        {
+            return Employee1.ID != Employee2.ID;
+        }
+
+        public override bool Equals(object o)
+        {
+            Employee EmpTmp = (Employee)o;
+            return ID == EmpTmp.ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ OverloadedOperator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee MyEmp1 = new Employee() { ID = 12345 };
+            Employee MyEmp2 = new Employee() { ID = 56789 };
+            Employee MyEmp3 = new Employee() { ID = 12345 };
+
+            if (MyEmp1 == MyEmp2)
+            {
+                Console.WriteLine("MyEmp1 == MyEmp2");
+            }
+            else if (MyEmp1 != MyEmp2)
+            {
+                Console.WriteLine("MyEmp1 != MyEmp2");
+            }
+
+            if (MyEmp1 == MyEmp3)
+            {
+                Console.WriteLine("MyEmp1 == MyEmp3");
+            }
+            else if (MyEmp1 != MyEmp3)
+            {
+                Console.WriteLine("MyEmp1 != MyEmp3");
+            }
+
+        }
+    }
+}
