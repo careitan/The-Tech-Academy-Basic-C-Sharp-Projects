bcd68c187dbdffc22c923504393bdca3e04c30bb
Allan Reitan <careitan2000@gmail.com>
2019-04-23 16:16:08 -0700

    DUN - Done
    DUN - Done

---
+++ Generics/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEmployee
+{
+    public class Employee<Type> : Person, IQuittable
+    {
+        public int ID;
+        public List<Type> Things;
+
+        public void Quit()
+        {
+            Console.WriteLine("{0} {1} says, 'I QUIT!'", this.FirstName, this.LastName);
+        }
+
+        //public static bool operator==(Employee Employee1, Employee Employee2)
+        //{
+        //    return Employee1.ID == Employee2.ID;
+        //}
+
+        //public static bool operator!=(Employee Employee1, Employee Employee2)
+        //{
+        //    return Employee1.ID != Employee2.ID;
+        //}
+
+        //public override bool Equals(object o)
+        //{
+        //    Employee EmpTmp = (Employee)o;
+        //    return ID == EmpTmp.ID;
+        //}
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> MyEmp1 = new Employee<string>();
+            Employee<int> MyEmp2 = new Employee<int>();
+
+            MyEmp1.Things = new List<string>() { "Coffee Mug", "Sliderule", "Red Swingline Stapler" };
+            MyEmp2.Things = new List<int>() { 2, 5, 7, 9 };
+
+            foreach (string thing in MyEmp1.Things)
+            {
+                Console.WriteLine("MyEmp1 thing is: {0}", thing);
+            }
+            Console.WriteLine();
+
+            foreach (int thing in MyEmp2.Things)
+            {
+                Console.WriteLine("MyEmp2 thing is: {0}", thing.ToString());
+            }
+
+        }
+    }
+}
