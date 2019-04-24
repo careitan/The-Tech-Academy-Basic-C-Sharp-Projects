0dff580fbb20b592e1dbf1925fea01c58a5b4325
Allan Reitan <careitan2000@gmail.com>
2019-04-23 17:31:17 -0700

    DUN - Done
    DUN - Done

---
+++ Enums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfWeek DOW = new DaysOfWeek();
+
+            Console.WriteLine("Please give me the current day of the week:");
+            try
+            {
+                DOW = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Enums/Weekdays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Weekdays
+    {
+    }
+    public enum DaysOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
