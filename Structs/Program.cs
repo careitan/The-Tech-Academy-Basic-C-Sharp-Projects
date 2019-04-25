6511e371387945ec6a75f599ab200ae63e51ce90
Allan Reitan <careitan2000@gmail.com>
2019-04-25 15:43:57 -0700

    DUN - Done
    DUN - Done

---
+++ Structs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number MyNum = new Number();
+
+            MyNum.Amount = 3.14159m;
+
+            Console.WriteLine("Here is a number from a struct {0}", MyNum.Amount.ToString());
+        }
+    }
+}
