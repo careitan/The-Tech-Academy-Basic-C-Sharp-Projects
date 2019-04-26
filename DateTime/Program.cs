3c219ed90525e5a1922ce5059cee188551df342e
Allan Reitan <careitan2000@gmail.com>
2019-04-26 16:21:31 -0700

    Staging
    Staging

---
+++ DateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\log.txt");
+
+            Console.WriteLine("The Current Date and Time is: " + System.DateTime.Now);
+            Console.WriteLine("Please give me a whole number: ");
+
+
+
+        }
+    }
+}
