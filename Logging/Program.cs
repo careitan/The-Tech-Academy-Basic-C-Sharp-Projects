52be56ba2c4d55e981607c4a23d4396fdaca3f29
Allan Reitan <careitan2000@gmail.com>
2019-04-26 15:58:13 -0700

    DUN -- Done
    DUN - Done

---
+++ Logging/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = System.Environment.CurrentDirectory + @"\log.txt";
+
+            int MyNum = int.MinValue;
+
+            Console.WriteLine("Please give me a number: ");
+            if (int.TryParse(Console.ReadLine(), out MyNum))
+            {
+                File.WriteAllText(filePath, MyNum.ToString());
+            }
+            else
+            {
+                throw new NotFiniteNumberException();
+            }
+
+            Console.WriteLine("... and after saving that number to a file here is what we have in the file:");
+            Console.WriteLine(File.ReadAllText(filePath));
+        }
+    }
+}
