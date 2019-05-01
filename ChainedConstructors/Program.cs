e8289bb420bb39baf8bacd3ed713e10f9456d103
Allan Reitan <careitan2000@gmail.com>
2019-05-01 13:57:07 -0700

    DUN - Done
    DUN - Done

---
+++ ChainedConstructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainedConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MyVar = "Hello World";
+            var TmpInput = string.Empty;
+
+            Console.WriteLine("please give me some type of input to work with.");
+            TmpInput = Console.ReadLine();
+
+            Something MyObj = new Something(TmpInput);
+
+            Console.WriteLine("The two properties of the Something Object are:");
+            Console.WriteLine("Something.Word = {0}",MyObj.Word);
+            Console.WriteLine("Something.Number = {0}", MyObj.Number);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ChainedConstructors/Something.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainedConstructors
+{
+    public class Something
+    {
+        public string Word { get; set; }
+        public int Number { get; set; }
+
+        public Something(int Input) : this(string.Empty, Input)
+        {
+        }
+
+        public Something(string Input) : this(Input, int.MinValue)
+        {
+        }
+
+        public Something(string Input, int Value)
+        {
+            Word = Input;
+            Number = Value;
+        }
+
+    }
+}
